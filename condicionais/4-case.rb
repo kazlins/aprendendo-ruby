09289c7ebc05eeef3e6f87ae076cf4f00e5e548f
Karyna Morais Lins <karynamolins@gmail.com>
2026-04-24 14:34:53 -0300

    Uso de case (condicionais)

---
+++ condicionais/4-case.rb
@@ -0,0 +1,13 @@
+# Uso de case como condicional, avaliando uma sentença
+cor = "amarelo"
+
+case cor
+when "amarelo"
+  puts "Cor do girassol"
+when "vermelho"
+  puts "Cor da rosa"
+when "rosa"
+  puts "Cor da orquídea"
+when "branco"
+  puts "Cor da margarida"
+end
