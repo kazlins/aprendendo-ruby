c5584656d0bad4dbdafd0bd46db67987f59fbd53
Karyna Morais Lins <karynamolins@gmail.com>
2026-04-24 13:55:41 -0300

    Variáveis e recebendo valores do usuário

---
+++ primeiro.rb
@@ -0,0 +1,13 @@
+# Criação de variáveis
+nome = "kayo"
+idade = 10
+
+# Diferença de print e puts
+print "hello #{nome}"
+puts "tem #{idade}"
+
+# Receber um valor do usuário
+outroNome = gets.chomp
+outraIdade = gets.chomp.to_i
+puts "hello também #{outroNome}"
+puts "tem #{outraIdade}"
