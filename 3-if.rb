f3f0284c857f800135851fcfe48ef19b108ff089
Karyna Morais Lins <karynamolins@gmail.com>
2026-04-24 14:30:20 -0300

    Uso de if, elsif e else (condicionais)

---
+++ 3-if.rb
@@ -0,0 +1,12 @@
+# Usando if pra avaliar se uma comparação é verdadeira
+x = 11
+
+if x == 10
+  puts "x realmente é 10"
+elsif x == 11
+  puts "x é igual a 11"
+else 
+  puts "x não é 10"
+end
+
+# Saída "x é igual a 11"
